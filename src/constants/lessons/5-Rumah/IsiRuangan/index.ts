04ee45c75f381227c7bfac6b94d6b7c82e741d33
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2023-03-16 04:18:36 +0700

    chore: cp

---
+++ src/constants/lessons/5-Rumah/IsiRuangan/index.ts
@@ -0,0 +1,250 @@
+import { LessonType } from '../../../Models/Lesson'
+
+export const IsiRuangan: LessonType[] = [
+  {
+    ar: 'بِرْكَةٌ',
+    id: 'Kolam',
+    img: require('../../../../../assets/lesson/5-2/1-kolam.png'),
+    sound: require('../../../../../assets/lesson/5-2/1-kolam.mp3')
+  },
+  {
+    ar: 'تَقْوِيْمٌ',
+    id: 'Kalender',
+    img: require('../../../../../assets/lesson/5-2/2-kalender.png'),
+    sound: require('../../../../../assets/lesson/5-2/2-kalender.mp3')
+  },
+  {
+    ar: 'هَاتِفٌ',
+    id: 'Telepon',
+    img: require('../../../../../assets/lesson/5-2/3-telepon.png'),
+    sound: require('../../../../../assets/lesson/5-2/3-telpon.mp3')
+  },
+  {
+    ar: 'تِلْفَازٌ',
+    id: 'TV',
+    img: require('../../../../../assets/lesson/5-2/4-tv.png'),
+    sound: require('../../../../../assets/lesson/5-2/4-TV.mp3')
+  },
+  {
+    ar: 'خِزَانَةٌ',
+    id: 'Lemari',
+    img: require('../../../../../assets/lesson/5-2/5-lemari.png'),
+    sound: require('../../../../../assets/lesson/5-2/5-lemari.mp3')
+  },
+  {
+    ar: 'رَفٌّ',
+    id: 'Rak',
+    img: require('../../../../../assets/lesson/5-2/6-rak.png'),
+    sound: require('../../../../../assets/lesson/5-2/6-rak.mp3')
+  },
+  {
+    ar: 'سَرِيْرٌ',
+    id: 'Ranjang',
+    img: require('../../../../../assets/lesson/5-2/7-ranjang.png'),
+    sound: require('../../../../../assets/lesson/5-2/7-ranjang.mp3')
+  },
+  {
+    ar: 'وِسَادَةٌ',
+    id: 'Bantal',
+    img: require('../../../../../assets/lesson/5-2/8-bantal.png'),
+    sound: require('../../../../../assets/lesson/5-2/8-bantal.mp3')
+  },
+  {
+    ar: 'شَجَرَةٌ',
+    id: 'Pohon',
+    img: require('../../../../../assets/lesson/5-2/9-pohon.png'),
+    sound: require('../../../../../assets/lesson/5-2/9-pohon.mp3')
+  },
+  {
+    ar: 'شُرْفَةٌ',
+    id: 'Teras/Balkon',
+    img: require('../../../../../assets/lesson/5-2/10-balkon.png'),
+    sound: require('../../../../../assets/lesson/5-2/10-balkon.mp3')
+  },
+  {
+    ar: 'صَابُوْنٌ',
+    id: 'Sabun',
+    img: require('../../../../../assets/lesson/5-2/11-sabun.png'),
+    sound: require('../../../../../assets/lesson/5-2/11-sabun.mp3')
+  },
+  {
+    ar: 'مِغْرَفَةٌ',
+    id: 'Gayung',
+    img: require('../../../../../assets/lesson/5-2/12-gayung.png'),
+    sound: require('../../../../../assets/lesson/5-2/12-gayung.mp3')
+  },
+  {
+    ar: 'مَائِدَةٌ',
+    id: 'Meja Makan',
+    img: require('../../../../../assets/lesson/5-2/13-mejamakan.png'),
+    sound: require('../../../../../assets/lesson/5-2/13-mejamakan.mp3')
+  },
+  {
+    ar: 'مِصِبَاحٌ',
+    id: 'Lampu',
+    img: require('../../../../../assets/lesson/5-2/14-lampu.png'),
+    sound: require('../../../../../assets/lesson/5-2/14-lampuu.mp3')
+  },
+  {
+    ar: 'صُوْرَةٌ',
+    id: 'Gambar',
+    img: require('../../../../../assets/lesson/5-2/15-gambar.png'),
+    sound: require('../../../../../assets/lesson/5-2/15-gambarr.mp3')
+  },
+  {
+    ar: 'مَكْتَبٌ',
+    id: 'Meja',
+    img: require('../../../../../assets/lesson/5-2/16-meja.png'),
+    sound: require('../../../../../assets/lesson/5-2/16-mejaa.mp3')
+  },
+  {
+    ar: 'مِنْضَدَةٌ',
+    id: 'Meja Tamu',
+    img: require('../../../../../assets/lesson/5-2/17-mejatamu.png'),
+    sound: require('../../../../../assets/lesson/5-2/17-mejatamu.mp3')
+  },
+  {
+    ar: 'كُرْسِيٌّ',
+    id: 'Kursi',
+    img: require('../../../../../assets/lesson/5-2/18-kursi.png'),
+    sound: require('../../../../../assets/lesson/5-2/18-kursii.mp3')
+  },
+  {
+    ar: 'مَلْبَسٌ',
+    id: 'Baju',
+    img: require('../../../../../assets/lesson/5-2/19-baju.png'),
+    sound: require('../../../../../assets/lesson/5-2/19-baju.mp3')
+  },
+  {
+    ar: 'سِرْوَالٌ',
+    id: 'Celana',
+    img: require('../../../../../assets/lesson/5-2/20-celana.png'),
+    sound: require('../../../../../assets/lesson/5-2/20-celana.mp3')
+  },
+  {
+    ar: 'سَاعَةٌ',
+    id: 'Jam',
+    img: require('../../../../../assets/lesson/5-2/21-jam.png'),
+    sound: require('../../../../../assets/lesson/5-2/21-jam.mp3')
+  },
+  {
+    ar: 'مِرْحَضٌ',
+    id: 'Toilet',
+    img: require('../../../../../assets/lesson/5-2/22-toilet.png'),
+    sound: require('../../../../../assets/lesson/5-2/22-toilett.mp3')
+  },
+  {
+    ar: 'فَاكْس',
+    id: 'Fax',
+    img: require('../../../../../assets/lesson/5-2/23-fax.png'),
+    sound: require('../../../../../assets/lesson/5-2/23-fax.mp3')
+  },
+  {
+    ar: 'مَاءٌ',
+    id: 'Air',
+    img: require('../../../../../assets/lesson/5-2/24-air.png'),
+    sound: require('../../../../../assets/lesson/5-2/24-air.mp3')
+  },
+  {
+    ar: 'شَاي',
+    id: 'Teh',
+    img: require('../../../../../assets/lesson/5-2/25-teh.png'),
+    sound: require('../../../../../assets/lesson/5-2/25-teh.mp3')
+  },
+  {
+    ar: 'قَهْوَةٌ',
+    id: 'Kopi',
+    img: require('../../../../../assets/lesson/5-2/26-kopi.png'),
+    sound: require('../../../../../assets/lesson/5-2/26-kopi.mp3')
+  },
+  {
+    ar: 'رُزٌّ',
+    id: 'Nasi',
+    img: require('../../../../../assets/lesson/5-2/27-nasi.png'),
+    sound: require('../../../../../assets/lesson/5-2/27-nasi.mp3')
+  },
+  {
+    ar: 'صَحْنٌ',
+    id: 'Piring',
+    img: require('../../../../../assets/lesson/5-2/28-piring.png'),
+    sound: require('../../../../../assets/lesson/5-2/28-piring.mp3')
+  },
+  {
+    ar: 'مِلْعَقَةٌ',
+    id: 'Garpu',
+    img: require('../../../../../assets/lesson/5-2/29-garpu.png'),
+    sound: require('../../../../../assets/lesson/5-2/29-garpu.mp3')
+  },
+  {
+    ar: 'قَلَمٌ',
+    id: 'Pena',
+    img: require('../../../../../assets/lesson/5-2/30-pena.png'),
+    sound: require('../../../../../assets/lesson/5-2/30-pena.mp3')
+  },
+  {
+    ar: 'مِرْسَمَةٌ',
+    id: 'Pensil',
+    img: require('../../../../../assets/lesson/5-2/31-pesnsil.png'),
+    sound: require('../../../../../assets/lesson/5-2/31-pensil.mp3')
+  },
+  {
+    ar: 'مقْلَمَةٌ',
+    id: 'Tempat Pena',
+    img: require('../../../../../assets/lesson/5-2/32-tempatpena.png'),
+    sound: require('../../../../../assets/lesson/5-2/32-tempatpensil.mp3')
+  },
+  {
+    ar: 'كِتَابٌ',
+    id: 'Buku',
+    img: require('../../../../../assets/lesson/5-2/33-buku.png'),
+    sound: require('../../../../../assets/lesson/5-2/33-buku.mp3')
+  },
+  {
+    ar: 'زَهْرَةٌ',
+    id: 'Bunga',
+    img: require('../../../../../assets/lesson/5-2/34-bunga.png'),
+    sound: require('../../../../../assets/lesson/5-2/34-bunga.mp3')
+  },
+  {
+    ar: 'زَهْرِيَّةٌ',
+    id: 'Vas',
+    img: require('../../../../../assets/lesson/5-2/35-vas.png'),
+    sound: require('../../../../../assets/lesson/5-2/35-vas.mp3')
+  },
+  {
+    ar: 'سُوْرٌ',
+    id: 'Pagar',
+    img: require('../../../../../assets/lesson/5-2/36-pagar.png'),
+    sound: require('../../../../../assets/lesson/5-2/36-pagar.mp3')
+  },
+  {
+    ar: 'قِرْمِيْدَةٌ',
+    id: 'Genteng',
+    img: require('../../../../../assets/lesson/5-2/37-genteng.png'),
+    sound: require('../../../../../assets/lesson/5-2/37-genteng.mp3')
+  },
+  {
+    ar: 'مِرْوَحَةٌ',
+    id: 'Kipas',
+    img: require('../../../../../assets/lesson/5-2/38-kipas.png'),
+    sound: require('../../../../../assets/lesson/5-2/38-kipas.mp3')
+  },
+  {
+    ar: 'مِكْنَسَةٌ',
+    id: 'Sapu',
+    img: require('../../../../../assets/lesson/5-2/39-sapu.png'),
+    sound: require('../../../../../assets/lesson/5-2/39-sapu.mp3')
+  },
+  {
+    ar: 'جَرِيْدَةٌ',
+    id: 'Koran',
+    img: require('../../../../../assets/lesson/5-2/40-koran.png'),
+    sound: require('../../../../../assets/lesson/5-2/40-koran.mp3')
+  },
+  {
+    ar: 'طَعَامٌ',
+    id: 'Makanan',
+    img: require('../../../../../assets/lesson/5-2/41-makan.png'),
+    sound: require('../../../../../assets/lesson/5-2/41-makanan.mp3')
+  }
+]
